74bd97ef5af6a19385a223e14f0493b084a14625
Abhishek Singh <jonu.1504@gmail.com>
2017-08-21 15:03:45 +0000

    async/await

--- controller/url-shortener.js
+++ controller/url-shortener.js
@@ -2,55 +2,61 @@ const mongoose = require('mongoose');
 const UrlMap = mongoose.model('Url');
 module.exports = {
 
-    shortenUrl: (req, res) => {
+    shortenUrl: async(req, res) => {
         let formattedUrl = req.params['0'].substring(0, req.params['0'].length);
         if (validateUrl(formattedUrl)) {
-            if (!/^(f|ht)tps?:\/\//i.test(formattedUrl)) {
-                formattedUrl = "http://" + formattedUrl;
+            if (!/^(f|ht)tps?:\/\//i.test(formattedUrl)) formattedUrl = "http://" + formattedUrl;
+            let orgUrl = await UrlMap.findOne({ "original_url": formattedUrl });
+            if (orgUrl) {
+                res.json({ "original_url": formattedUrl, "short_url": orgUrl.short_url });
             }
-            UrlMap.findOne({ "original_url": formattedUrl }).then((orgUrl) => {
-                if (orgUrl) {
-                    res.json({ "original_url": formattedUrl, "short_url": orgUrl.short_url });
-                }
-                else {
+            else {
+                try {
                     const shortCode = Math.floor(Math.random() * (9999 - 1000) + 1000);
                     const shortUrl = req.headers['x-forwarded-proto'] + '://' + req.headers['host'] + '/' + shortCode;
                     const obj = { "original_url": formattedUrl, "short_url": shortUrl };
                     let urlMaped = new UrlMap(obj);
                     urlMaped.shorten_url_id = shortCode;
-                    urlMaped.save().then((data) => {
-                        res.json(obj);
-                    }).catch((error) => {
-                        console.error("there is error in generating url", error);
-                        res.json({ "error": "there is error in generating url" });
-                    });
+                    await urlMaped.save();
                 }
-            });
+                catch (e) {
+                    console.error("there is error in generating url", e.message);
+                    res.json({ "error": "there is error in generating url" });
+                }
+
+            }
         }
         else {
             res.json({ "error": "Wrong Url Format." });
         }
     },
 
-    getoriginalUrl: (req, res) => {
-        UrlMap.findOne({ "shorten_url_id": req.params.shortCode }).then((urlObj) => {
-            if (urlObj) {
-                UrlMap.update({ "shorten_url_id": req.params.shortCode }, { $inc: { views: 1 } }).then(() => {
-                    res.redirect(urlObj.original_url);
-                });
+    getoriginalUrl: async(req, res) => {
+
+        let urlObj = await UrlMap.findOne({ "shorten_url_id": req.params.shortCode });
+        if (urlObj) {
+            try {
+                await UrlMap.update({ "shorten_url_id": req.params.shortCode }, { $inc: { views: 1 } });
             }
-            else {
-                res.json({ "error": "No Url Found." });
+            catch (e) {
+                console.log('error', e.message);
             }
-        }).catch((error) => {
-            console.error("there is error in findind short url to original_url", error);
-            res.json({ "error": "there is error in findind short url to original_url" });
-        });
+            finally {
+                res.redirect(urlObj.original_url);
+            }
+        }
+        else {
+            res.json({ "error": "No Url Found." });
+        }
     },
-    getLastGeneratedUrl: (req, res) => {
-        UrlMap.find({}).sort({ createdAt: -1 }).limit(5).then((urls) => {
-            res.json(urls);
-        }).catch(()=> res.json({}));
+    getLastGeneratedUrl: async(req, res) => {
+        try {
+            let urls = await UrlMap.find({}).sort({ createdAt: -1 }).limit(5);
+            urls?res.json(urls):res.json({});
+        }
+        catch (e) {
+            res.json({});
+        }
     }
 
 };
